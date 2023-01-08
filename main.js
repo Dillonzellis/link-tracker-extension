2f1d183d60f354cfaf99dfc012545673643fb732
Dillon Ellis <dillonzellis@gmail.com>
2023-01-08 18:29:57 -0500

    full functionality

--- main.js
+++ main.js
@@ -1,36 +1,58 @@
-let myLeads = [];
-
+let myLeads = []
 
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
+const delBtn = document.getElementById("del-btn");
 const inputEl = document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el");
 
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+
 
 inputBtn.addEventListener('click', getInput)
+tabBtn.addEventListener('click', saveTab)
+delBtn.addEventListener('click', clearData)
 
 
-function getInput(){
-    myLeads.push(inputEl.value)
-    inputEl.value = "";
-    renderLeads()
+
+function getInput() {
+    if (inputEl.value != "") {
+        myLeads.push(inputEl.value)
+        inputEl.value = "";
+
+        localStorage.setItem('myLeads', JSON.stringify(myLeads))
+        render(myLeads)
+    }
+}
+
+function saveTab() {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem('myLeads', JSON.stringify(myLeads))
+        render(myLeads)
+    })
+
+
+}
+
+function clearData() {
+    localStorage.clear()
+    myLeads = [];
+    render(myLeads)
 }
 
 
-// myLeads.map(leads => ulEl.innerHTML += ("<li>" + leads + "</li>"))
-
-function renderLeads() {
+function render(list) {
     let listItems = "";
-    myLeads.map(leads => listItems += (`<li><a href="${leads}" target="_blank"> ${leads} </a></li>`) )
+    list.map(items => listItems += (`<li><a href="${items}" target="_blank"> ${items} </a></li>`))
     ulEl.innerHTML = listItems;
-    
 }
-
-
-// for(let i  = 0; i <myLeads.length; i++){
-//     ulEl.innerHTML += "<li>" "</li>"
-// }
-
-
-
-
-
